6a21303af0a77ce98a63e08c3298b5c82892ae5e
Ana Marr <acarolinasim@gmail.com>
2020-03-14 23:25:21 +1030

    java quiz intial page

---
+++ script.js
@@ -0,0 +1,11 @@
+var timerEl = document.getElementById("Timer");
+var highScoreEl = document.getElementById("highScore");
+var questionEl = document.getElementById("questionHeader");
+var choiceEl = document.getElementById("quizChoice");
+var btn = document.getElementById("btnSubmit");
+
+var questionArray = []
+var answerObject = {
+
+};
+var score = 0;
